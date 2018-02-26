bd9b2b525c1a282baa782f39fb5811f6f8a8c2aa
naoishii <naoishii.dev@gmail.com>
2018-02-26 12:03:01 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "standard",
+  "plugins": [
+    "react",
+    "prettier",
+    "standard",
+    "promise"
+  ],
+  "rules": {
+    "prettier/prettier": "error"
+  }
+};

9fae8c90190cbaa3ef66d3dc1545a0887af2eb9b
naoishii <naoishii.dev@gmail.com>
2018-02-26 19:16:59 +0900

    redirectを受け取るハンドラはauthStateChangeがあるので書かなくてよかった

--- src/firebase.js
+++ src/firebase.js
@@ -26,20 +26,6 @@ const connectFirebase = store => {
       store.dispatch(userAction.logout(uid));
     }
   });
-
-  firebase
-    .auth()
-    .getRedirectResult()
-    .then(result => {
-      // リダイレクトにより認証が完了した場合、↑の onAuthStateChanged が呼ばれるのでそちらで処理を行います
-      // if (result.credential) {
-      //   // This gives you a Google Access Token. You can use it to access the Google API.
-      //   const token = result.credential.accessToken;
-      // }
-      // // The signed-in user info.
-      // const user = result.user;
-      // const uid = user.uid;
-    });
 };
 
 export default firebaseApp;

3f7a91d083132b6861e2487ad30b14ad68cd822f
naoishii <naoishii.dev@gmail.com>
2018-02-27 20:02:58 +0900

    functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.checkMessage = functions.firestore
+  .document('chat/{id}')
+  .onCreate(event => {
+    const newValue = event.data.data();
+
+    // access a particular field as you would any JS property
+    const username = newValue.username;
+    const message = newValue.message;
+    const date = newValue.date;
+
+    // perform desired operations ...
+    if (message.includes('うんち') || message.includes('test')) {
+      return event.data.ref.set({
+        username,
+        message: 'あぼーん',
+        date,
+      });
+    }
+
+    return null;
+  });
